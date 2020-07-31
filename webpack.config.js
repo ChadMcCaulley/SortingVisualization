428f6f24281a9fb98435c7403f6bf1d556371d23
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-30 20:23:38 -0700

    Fixed templating

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,44 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       appMountId: 'app',
-      filename: 'index.html'
+      filename: 'index.html',
+      title: 'Sorting Visualization',
+      templateContent: `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8" />
+            <title>Sorting Visualized</title>
+          </head>
+          <body>
+            <div class="navbar">
+              <button
+                class="btn btn-text"
+                style="margin-left: 1em"
+                id="randomize-button"
+              >
+                Randomize
+              </button>
+              <div>
+                <button id="merge-button" class="btn btn-option">
+                  Merge Sort
+                </button>
+                <button id="heap-button" class="btn btn-option">
+                  Heap Sort
+                </button>
+                <button id="quick-button" class="btn btn-option">
+                  Quick Sort
+                </button>
+                <button id="bubble-button" class="btn btn-option">
+                  Bubble Sort
+                </button>
+              </div>
+            </div>
+            <div id="array-container" class="array-container">
+            </div>
+          </body>
+        </html>
+      `
     })
   ]
 }
